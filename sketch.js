c3be8cfd2a9f1afe14dac886acb34c742608e3fb
Christian Biasi <christianbiasi@gmx.de>
2019-01-18 11:48:46 +0100

    tried online solution , not really working yet

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ let counter = 1;
 function setup() {
   createCanvas(100, 100, WEBGL);
   // use an angle of degrees, otherwise need to use radians with radians(degrees) for conversion
-  angleMode(DEGREES);
+  //angleMode(DEGREES);
 }
 
 function draw() {
@@ -14,34 +14,35 @@ function draw() {
   angle = str(counter++);
 
   // angle as new rotation position
-  rotateX(parseInt(angle));
-  box(50);
-
+  
   // if angle greater than 360 degrees then reset to 0, just cosmetic effect yet, might just be for visible correctness...
-  if(angle <= 359){
+  /* if(angle <= 359){
     angle++;
   } else {
     angle = 0;
     counter = 1;
-  }
-
+  } */
+  
   // just to document angle change visible as text
-  document.getElementById("title1").innerHTML = angle;
-
+  //document.getElementById("title1").innerHTML = angle;
+  
   /* code below commented out as no connection to pi available 
   ----------------------------------------------------------- */
-
+  
   // AJAX request
-  /* var xhr = new XMLHttpRequest();
-  xhr.open('GET', "https://iybzjdzs.p55.rt3.io/", true);
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', "https://otmdgwbz.p50.rt3.io/", true);
   xhr.send();
   
   xhr.onreadystatechange = function () {
     if (xhr.readyState == 4 && xhr.status == 200) {
       var data = xhr.responseText.split(" ");
+      box(50);
       document.getElementById("title1").innerHTML = data[0];
+      rotateX(cos(parseInt(data[0])));
       document.getElementById("title2").innerHTML = data[1];
+      rotateY(parseInt(data[1]));
       document.getElementById("title3").innerHTML = data[2];
     }
-  }; */
+  };
 }
