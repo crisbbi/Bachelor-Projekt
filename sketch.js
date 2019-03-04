6580019ec6a8de48e1bd37a394fc3f2a1c32994b
Christian Biasi <christianbiasi@gmx.de>
2019-03-04 01:30:49 +0100

    reset code to simplest start

--- sketch.js
+++ sketch.js
@@ -9,40 +9,20 @@ function setup() {
 
 function draw() {
   background(200);
-
-  // simulate data received as string from ajax, parse back to usable number
-  angle = str(counter++);
-
-  // angle as new rotation position
-  
-  // if angle greater than 360 degrees then reset to 0, just cosmetic effect yet, might just be for visible correctness...
-  /* if(angle <= 359){
-    angle++;
-  } else {
-    angle = 0;
-    counter = 1;
-  } */
   
   // just to document angle change visible as text
   //document.getElementById("title1").innerHTML = angle;
   
-  /* code below commented out as no connection to pi available 
-  ----------------------------------------------------------- */
-  
   // AJAX request
   var xhr = new XMLHttpRequest();
-  xhr.open('GET', "https://otmdgwbz.p50.rt3.io/", true);
+  xhr.open('GET', "https://erbksaly.p50.rt3.io/", true);
   xhr.send();
   
   xhr.onreadystatechange = function () {
     if (xhr.readyState == 4 && xhr.status == 200) {
-      var data = xhr.responseText.split(" ");
-      box(50);
+      var data = xhr.responseText.split(",");
       document.getElementById("title1").innerHTML = data[0];
-      rotateX(cos(parseInt(data[0])));
       document.getElementById("title2").innerHTML = data[1];
-      rotateY(parseInt(data[1]));
-      document.getElementById("title3").innerHTML = data[2];
     }
   };
 }
